1f0d4912ff4d21941c775b939e37a5fb9e72b89d
MDX-gamer <54910479+MDX-gamer@users.noreply.github.com>
2020-08-14 19:35:26 +0530

    ar_Foundation

---
+++ New Unity Project/Assets/Scripts/AR_Foundation/AR_Translate.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AR_Translate : MonoBehaviour
+{
+    //// float deltaX, deltaY;
+    //Vector3 dist;
+    //float posX;
+    //float posY;
+
+    //void OnMouseDown()
+    //{
+    //    dist = Camera.main.WorldToScreenPoint(transform.position);
+    //    posX = Input.mousePosition.x - dist.x;
+    //    posY = Input.mousePosition.y - dist.y;
+
+    //}
+
+    //void OnMouseDrag()
+    //{
+    //    Vector3 curPos = new Vector3(Input.mousePosition.x-posX, dist.y,Input.mousePosition.y- posY);
+
+    //    Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+    //    transform.position = worldPos;
+
+    //}
+
+    //private void OnMouseDrag()
+    //{
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+    //        
+    //        
+    //        if (touch.phase == TouchPhase.Moved)
+    //        {
+    //            
+    //           // transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * 0.007f, transform.position.y, transform.position.z + touch.deltaPosition.y * 0.007f);
+    //        }
+    //    }
+    //}
+
+    private Vector3 moffset;
+    private float mZcoord;
+
+    private void OnMouseDown()
+    {
+        mZcoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        moffset = gameObject.transform.position - GetMouseworldPos();
+    }
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseworldPos() + moffset;
+    }
+    private Vector3 GetMouseworldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZcoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/AR_Foundation/rotateController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class rotateController : MonoBehaviour 
+{
+
+	#region ROTATE
+	private float _sensitivity = 0.5f;
+	private Vector3 _mouseReference;
+	private Vector3 _mouseOffset;
+	private Vector3 _rotation = Vector3.zero;
+	private bool _isRotating;
+
+
+	#endregion
+
+	void Update()
+	{
+		if(_isRotating)
+		{
+			_mouseOffset = (Input.mousePosition - _mouseReference);
+			_rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+			gameObject.transform.Rotate(_rotation);
+			_mouseReference = Input.mousePosition;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		_isRotating = true;
+		_mouseReference = Input.mousePosition;
+	}
+
+	void OnMouseUp()
+	{
+		_isRotating = false;
+	}
+
+}
